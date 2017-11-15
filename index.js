2e5dd0151370ce0f1dbac7150b27dc56e8976c72
prosunshining <prosunshining@gmail.com>
2017-11-15 07:03:12 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+  console.log(req.headers);
+  res.send('hello world');
+});
+
+app.get('/@:userhandler', function(req, res) {
+  res.redirect('http://beta.speev.com/' + req.params.userhandler);
+})
+
+app.listen(3000);
